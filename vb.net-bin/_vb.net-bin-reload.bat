5f45c971b50f63366529b36d06e5151544b4abb2
asm32cn <asm32@qq.com>
2018-05-10 00:38:58 +0800

    _vb.net-bin-reload.bat ... add

---
+++ vb.net-bin/_vb.net-bin-reload.bat
@@ -0,0 +1,43 @@
+@set file1=..\vb.net-2016\PaTriangle2016VB20.vb\PaTriangle2016VB20.exe
+@echo %file1%
+@if exist "%file1%" copy "%file1%"
+
+@set file1=..\vb.net-2016\PaStars2016VB20.vb\PaStars2016VB20.exe
+@echo %file1%
+@if exist "%file1%" copy "%file1%"
+
+@set file1=..\vb.net-2016\PaStarry2016VB20.vb\PaStarry2016VB20.exe
+@echo %file1%
+@if exist "%file1%" copy "%file1%"
+
+@set file1=..\vb.net-2016\PaSin2016VB20.vb\PaSin2016VB20.exe
+@echo %file1%
+@if exist "%file1%" copy "%file1%"
+
+@set file1=..\vb.net-2016\PaQix2016VB20.vb\PaQix2016VB20.exe
+@echo %file1%
+@if exist "%file1%" copy "%file1%"
+
+@set file1=..\vb.net-2016\PaEllipse2016VB20.vb\PaEllipse2016VB20.exe
+@echo %file1%
+@if exist "%file1%" copy "%file1%"
+
+@set file1=..\vb.net-2016\PaDisplayHZ2016VB20.vb\PaDisplayHZ2016VB20.exe
+@echo %file1%
+@if exist "%file1%" copy "%file1%"
+
+@set file1=..\vb.net-2016\PaDigitalClock2016VB20.vb\PaDigitalClock2016VB20.exe
+@echo %file1%
+@if exist "%file1%" copy "%file1%"
+
+@set file1=..\vb.net-2016\PaClock2016VB20.vb\PaClock2016VB20.exe
+@echo %file1%
+@if exist "%file1%" copy "%file1%"
+
+@set file1=..\vb.net-2016\PaBalls2016VB20.vb\PaBalls2016VB20.exe
+@echo %file1%
+@if exist "%file1%" copy "%file1%"
+
+@set file1=
+
+@pause
