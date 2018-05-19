3074735d0f5b0e29ca63ad4e626282314354d6b7
asm32 <asm32@qq.com>
2018-05-19 17:45:14 +0800

    vb-bucket-sort-demo-1--linker.bat ... add

---
+++ vb.net-Arithmetic-2018/vb-bucket-sort-demo-1.vb/vb-bucket-sort-demo-1--linker.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set strLinker=%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\vbc.exe
+set strAppName=vb-bucket-sort-demo-1
+echo %strLinker%
+echo.
+
+set strCmd=%strLinker% /target:exe /optimize %strAppName%.vb
+
+echo #%strCmd%
+%strCmd%
+
+pause
